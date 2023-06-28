38273c79bde73cdf14a7f21b550d830fa0a7bad5
qqwy <qqwy@gmx.com>
2023-06-28 09:33:24 +0200

    Running mix init

---
+++ lib/type_check_compile_error.ex
@@ -0,0 +1,18 @@
+defmodule TypeCheckCompileError do
+  @moduledoc """
+  Documentation for `TypeCheckCompileError`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TypeCheckCompileError.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/type_check_compile_error_test.exs
@@ -0,0 +1,8 @@
+defmodule TypeCheckCompileErrorTest do
+  use ExUnit.Case
+  doctest TypeCheckCompileError
+
+  test "greets the world" do
+    assert TypeCheckCompileError.hello() == :world
+  end
+end

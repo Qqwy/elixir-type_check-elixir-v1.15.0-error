ccd7268cd379557c4a0016df0593d4e8e7ecb153
qqwy <qqwy@gmx.com>
2023-06-29 11:32:12 +0200

    Make example code clearer

--- lib/main.ex
+++ lib/main.ex
@@ -2,7 +2,6 @@ defmodule Main do
   import Macros
 
   if_recompiling? do
-    IO.puts "Second time compiling"
     use Main
   else
     IO.puts "First time compiling"
@@ -11,8 +10,8 @@ defmodule Main do
   defmacro __using__(_options) do
     IO.puts "__using__ was called"
     quote do
-      def foo do
-        "bar"
+      def added do
+        "I was added later"
       end
     end
   end
